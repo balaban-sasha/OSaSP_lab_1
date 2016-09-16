955ec0a2511f1f2ed72595341a8a5de7c1aa3ea4
MSI-PC <MSI-PC@DESKTOP-QESS1M1>
2016-09-16 10:48:10 +0300

    full lab

--- lab_1/Shape.h
+++ lab_1/Shape.h
@@ -4,13 +4,16 @@
 #include "lab_1.h"
 #include<vector>
 #include<list>	
+#include <commdlg.h>
 class Shapes
 {
-public:
-	Shapes() {};
+protected:
 	int x, y, cx, cy;
-
+	HPEN hpen;
+	HBRUSH brush;
 public:
+	virtual void SetHpen(HPEN) {};
+	virtual void SetBrush(COLORREF) {};
 	virtual void SetStartPosition(int,int)
 	{
 	}
@@ -19,8 +22,12 @@ public:
 	}
 	virtual void AddPointToArray(int, int)
 	{
-			}
-	virtual void Draw(HDC,HWND){
+	}
+
+	virtual void SetText(WPARAM, int)
+	{
+	}
+	virtual void Draw(HDC){
 	}
 };
 class MyRectangle:public Shapes
@@ -38,8 +45,18 @@ public:
 		cx = x2;
 		cy = y2;
 	}
-	void Draw(HDC hdc, HWND hWnd)
+	void SetHpen(HPEN hPen)
 	{
+		hpen = hPen;
+	}
+	void SetBrush(COLORREF hBrush)
+	{
+		brush = (HBRUSH)CreateSolidBrush(hBrush);
+	}
+	void Draw(HDC hdc)
+	{
+		SelectObject(hdc, brush);
+			SelectObject(hdc, hpen);
 		Rectangle(hdc, x, y, cx, cy);
 	}
 };
@@ -58,8 +75,18 @@ public:
 		cx = x2;
 		cy = y2;
 	}
-	void Draw(HDC hdc, HWND hWnd)
+	void SetBrush(COLORREF hBrush)
 	{
+		brush = (HBRUSH)CreateSolidBrush(hBrush);
+	}
+	void SetHpen(HPEN hPen)
+	{
+		hpen = hPen;
+	}
+	void Draw(HDC hdc)
+	{
+		SelectObject(hdc, brush);
+		SelectObject(hdc, hpen);
 		Ellipse(hdc, x, y, cx, cy);
 	}
 };
@@ -78,13 +105,18 @@ public:
 		cx = x2;
 		cy = y2;
 	}
-	void Draw(HDC hdc, HWND hWnd)
+	void SetHpen(HPEN hPen)
 	{
+		hpen = hPen;
+	}
+	void Draw(HDC hdc)
+	{
+		SelectObject(hdc, hpen);
 		MoveToEx(hdc, x, y,NULL);
 		LineTo(hdc, cx, cy);
 	}
 };
-class MyPan:public Shapes
+class MyPen:public Shapes
 {
 private:
 	std::vector <POINT> points;
@@ -97,8 +129,13 @@ public:
 		tempPoint.y=y1;
 		points.push_back(tempPoint);
 	}
-	void Draw(HDC hdc, HWND hWnd)
+	void SetHpen(HPEN hPen)
 	{
+		hpen = hPen;
+	}
+	void Draw(HDC hdc)
+	{
+		SelectObject(hdc, hpen);
 		POINT temp;
 		if (points.size() > 1)
 		{
@@ -113,5 +150,131 @@ public:
 		else
 			SetPixel(hdc, points[0].x, points[0].y, RGB(0, 0, 0));
 		
+	}
+};
+class MyPolygone :public Shapes
+{
+private:
+	std::vector <POINT> points;
+public:
+	using Shapes::Shapes;
+	void AddPointToArray(int x1, int y1)
+	{
+		POINT tempPoint;
+		tempPoint.x = x1;
+		tempPoint.y = y1;
+		points.push_back(tempPoint);
+	}
+	void SetHpen(HPEN hPen)
+	{
+		hpen = hPen;
+	}
+	void SetBrush(COLORREF hBrush)
+	{
+		brush = (HBRUSH)CreateSolidBrush(hBrush);
+	}
+	void Draw(HDC hdc)
+	{
+		SelectObject(hdc, brush);
+		SelectObject(hdc, hpen);
+		Polygon(hdc, points.data(), points.size());
+
+	}
+};
+class MyPolyLine :public Shapes
+{
+private:
+	std::vector <POINT> points;
+public:
+	using Shapes::Shapes;
+	void AddPointToArray(int x1, int y1)
+	{
+		POINT tempPoint;
+		tempPoint.x = x1;
+		tempPoint.y = y1;
+		points.push_back(tempPoint);
+	}
+	void SetHpen(HPEN hPen)
+	{
+		hpen = hPen;
+	}
+	void Draw(HDC hdc)
+	{
+		SelectObject(hdc, hpen);
+		Polyline(hdc, points.data(), points.size());
+
+	}
+};
+class MyText :public Shapes
+{
+public:
+	 int size=0;
+	 COLORREF color;
+	 PTCHAR text=(PTCHAR)GlobalAlloc(GPTR, 50000 * sizeof(TCHAR));
+public:
+	using Shapes::Shapes;
+
+	void SetStartPosition(int x1, int y1)
+	{
+		x = x1;
+		y = y1;
+	}
+	void setHpen(HPEN hPen)
+	{
+		hpen = hPen;
+	}
+	void SetBrush(COLORREF hBrush)
+	{
+		color = hBrush;
+	}
+	void SetText(WPARAM wParam,int newSize)
+	{
+		text[size] = (char)wParam;
+		size = newSize;
+	}
+	void Draw(HDC hdc)
+	{
+		RECT rec; 
+		SetRect(&rec, x, y, x+10*size, y+30);
+		SelectObject(hdc, hpen);
+		SetTextColor(hdc, color);
+	DrawText(hdc, (LPCWSTR)text, size, &rec, DT_TOP | DT_LEFT);
+
+	}
+};
+class MyEraser :public Shapes
+{
+private:
+	std::vector <POINT> points;
+public:
+	using Shapes::Shapes;
+	void AddPointToArray(int x1, int y1)
+	{
+		POINT tempPoint;
+		tempPoint.x = x1;
+		tempPoint.y = y1;
+		points.push_back(tempPoint);
+	}
+	void SetHpen(HPEN hPen)
+	{
+		hpen = hPen;
+	}
+	void Draw(HDC hdc)
+	{
+		SelectObject(hdc, hpen);
+		POINT temp;
+		if (points.size() > 1)
+		{
+			temp.x = points[0].x;
+			temp.y = points[0].y;
+			MoveToEx(hdc, temp.x, temp.y, NULL);
+			for (int i = 1; i < points.size(); i++)
+			{
+				LineTo(hdc, points[i].x, points[i].y);
+			}
+		}
+		else
+			SetPixel(hdc, points[0].x, points[0].y, RGB(0, 0, 0));
+
 	}
 };
--- lab_1/lab_1.cpp
+++ lab_1/lab_1.cpp
@@ -13,6 +13,8 @@
 #include<memory>
 #include "fabric.h"
 #include <string>
+#include <commdlg.h>
+#include <windows.h>
 #define MAX_LOADSTRING 100
 using namespace std;
 
@@ -25,8 +27,8 @@ std::list <Shapes*> shapesList;
 int eqw = 312312;
 HWND button1;
 int what_need_to_paint;//1-прямая,2-Карандаш,3-Ломаная,4-Прямоугольник,5-Эллипс,6-Многоугольник,7-Текст
-
-std::string checkedShape="pan";
+//checkedShape is use that to know what shape we want to draw, default it is pen
+std::string checkedShape="pen";
 #define ID_COMBODAY 2
 // Глобальные переменные:
 HINSTANCE hInst;                                // текущий экземпляр
@@ -37,10 +39,8 @@ ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-void CreateButton(HWND hWnd, HWND button1, int x, int y, int cx, int cy, HMENU hmenu, LPCWSTR str)
-{
-	button1 = CreateWindow(L"button", str, WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, x, y, cx, cy, hWnd, (HMENU)hmenu, (HINSTANCE)GetWindowLong(hWnd, GWLP_HINSTANCE), NULL);
-}
+INT_PTR CALLBACK    DlgProc(HWND, UINT, WPARAM, LPARAM);
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -68,7 +68,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	MSG msg;
 
 	// Цикл основного сообщения:
-	while (GetMessage(&msg, nullptr, 0, 0))
+		while (GetMessage(&msg, nullptr, 0, 0))
 	{
 		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
 		{
@@ -119,26 +119,17 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
+
 	what_need_to_paint = 2;
 	hInst = hInstance; // Сохранить дескриптор экземпляра в глобальной переменной
 
-	HWND hWnd = /*CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);*/
-		CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN, CW_USEDEFAULT, 0, CW_USEDEFAULT, 
+	HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN, CW_USEDEFAULT, 0, CW_USEDEFAULT, 
 			0, NULL, NULL, hInstance, NULL);
 
 	if (!hWnd)
 	{
 		return FALSE;
 	}
-	CreateButton(hWnd, button1, 10, 10, 70, 20, (HMENU)1001, L"Прямая");
-	CreateButton(hWnd, button1, 90, 10, 80, 20, (HMENU)1002, L"Карандаш");
-	CreateButton(hWnd, button1, 180, 10, 70, 20, (HMENU)1003, L"Ломаная");
-	CreateButton(hWnd, button1, 260, 10, 120, 20, (HMENU)1004, L"Прямоугольник");
-	CreateButton(hWnd, button1, 390, 10, 70, 20, (HMENU)1005, L"Эллипс");
-	CreateButton(hWnd, button1, 470, 10, 120, 20, (HMENU)1006, L"Многоугольник");
-	CreateButton(hWnd, button1, 600, 10, 70, 20, (HMENU)1007, L"Текст");
-
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
@@ -155,162 +146,455 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY — отправить сообщение о выходе и вернуться
 //
 //
+//initialization of new variables
 HPEN hPen;
 BOOL fDraw = FALSE;
 POINT ptPrevious = { 0 };
 int points_num = 0;
-std::vector<POINT> pt;  Shapes* shape;
-GenericObjectFactory<string, Shapes> animalFactory;
+std::vector<POINT> pt;  
+Shapes* shape;
+GenericObjectFactory<string, Shapes> shapesFactory;
 PAINTSTRUCT ps;
 HDC hdc,hdcMem=0;
-
+COLORREF brush, brushFill=RGB(255,255,255);
+int penSize=1;
+RECT rec;
+HENHMETAFILE hMTF;
+static HBRUSH hBrush;
+static OPENFILENAME ofn;
+static char fullpath[256], filename[256], dir[256];
+ENHMETAHEADER emh;
+RECT rect, rect1;
+float coefficientOfSizeHeight =1, coefficientOfSizeWidth = 1;
+int shearX, shearY;
+int zDelta;
+bool needToFill = false;
+bool needToZoom = false;
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	static PTCHAR text;
+	static int size = 0;
 	switch (message)
 	{
+	case WM_KEYDOWN:
+		//delete last shape
+		if(!shapesList.empty())
+		if ((GetAsyncKeyState(VK_CONTROL)) && (GetAsyncKeyState(VK_F1)))
+			shapesList.pop_back();
+		break;
+	case WM_MOUSEWHEEL:
+	{
+		//use that to manage zoom
+		if (needToZoom) {
+			if ((GetAsyncKeyState(VK_CONTROL)) && (!GetAsyncKeyState(VK_SHIFT)))
+			{
+				if ((fDraw) && (what_need_to_paint != 7))
+					fDraw = false;
+				zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+				if ((zDelta < 0) && (coefficientOfSizeHeight < 3))
+					coefficientOfSizeHeight += 0.2;
+				else
+					if ((zDelta > 0) && (coefficientOfSizeHeight > 0.6))
+						coefficientOfSizeHeight -= 0.2;
+				needToFill = true;
+			}
+			if ((GetAsyncKeyState(VK_CONTROL)) && (GetAsyncKeyState(VK_SHIFT)))
+			{
+				if ((fDraw) && (what_need_to_paint != 7))
+					fDraw = false;
+				zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+				if ((zDelta < 0) && (coefficientOfSizeWidth < 3))
+					coefficientOfSizeWidth += 0.2;
+				else
+					if ((zDelta > 0) && (coefficientOfSizeWidth > 0.6))
+						coefficientOfSizeWidth -= 0.2;
+				needToFill = true;
+			}
+		}
+		else
+		{
+			//use that to manage pen
+			if (GetAsyncKeyState(VK_SHIFT))
+			{
+				if ((fDraw) && (what_need_to_paint != 7))
+					fDraw = false;
+				zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+				if ((zDelta < 0))
+					shearX += 1;
+				else
+					if ((zDelta > 0))
+						shearX -= 1;
+				needToFill = true;
+			}
+			if (!GetAsyncKeyState(VK_SHIFT))
+			{
+				if ((fDraw) && (what_need_to_paint != 7))
+					fDraw = false;
+				zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+				if ((zDelta < 0))
+					shearY += 1;
+				else
+					if ((zDelta > 0))
+						shearY -= 1;
+				needToFill = true;
+			}
+		}
+	}break;
+
 	case WM_ERASEBKGND:
 		return 1;
 	case WM_CREATE:
-			animalFactory.add<MyRectangle>("rectangle");
-			animalFactory.add<MyEllipse>("ellipse");
-			animalFactory.add<MyLine>("line");
-			animalFactory.add<MyPan>("pan");
-			SetTimer(hWnd, 1, 55, NULL);
-			//hPen = CreatePen(10, 10, BLACKNESS);
+			//add shapes to factory
+			shapesFactory.add<MyRectangle>("rectangle");
+			shapesFactory.add<MyEllipse>("ellipse");
+			shapesFactory.add<MyLine>("line");
+			shapesFactory.add<MyPen>("pen");
+			shapesFactory.add<MyPolyLine>("polyline");
+			shapesFactory.add<MyPolygone>("polygone");
+			shapesFactory.add<MyText>("text");
+			shapesFactory.add<MyEraser>("eraser");
+
+			SetTimer(hWnd, 1, 55, NULL); 
+			hPen = CreatePen(BS_SOLID, 1, NULL);
+			text = (PTCHAR)GlobalAlloc(GPTR, 50000 * sizeof(TCHAR));
 			break;
 	case WM_TIMER:
+		//that to redraw window
 		InvalidateRect(hWnd, NULL, true);
 		UpdateWindow(hWnd);
 		break;
 	case WM_INITDIALOG:
-		//bRet = TRUE;
-
 		break;
 	case WM_COMMAND:
 	{
 		int wmId = LOWORD(wParam);
-		// Разобрать выбор в меню:
 		switch (wmId)
 		{
 		case IDM_ABOUT:
 			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
+
 		case IDM_EXIT:
 			DestroyWindow(hWnd);
 			break;
-		case 1001:
+			//8 cases below are use that to choose new shape
+		case 32779:
 			what_need_to_paint = 1;
 			checkedShape = "line";
 			fDraw = FALSE;
 			break;
-		case 1002:
+		case 32773:
 			what_need_to_paint = 2;
-			checkedShape = "pan";
+			checkedShape = "pen";
 			fDraw = FALSE;
 			break;
-		case 1003:
+		case 32774:
 			what_need_to_paint = 3;
+			checkedShape = "polyline";
 			fDraw = FALSE;
 			break;
-		case 1004:
+		case 32775:
 			what_need_to_paint = 4;
 			checkedShape = "rectangle";
 			fDraw = FALSE;
 			break;
-		case 1005:
+	
+		case 32776:
 			what_need_to_paint = 5;
 			checkedShape = "ellipse";
 			fDraw = FALSE;
 			break;
-		case 1006:
+		case 32777:
 			what_need_to_paint = 6;
+			checkedShape = "polygone";
 			fDraw = FALSE;
 			break;
-		case 1007:
+		case 32778:
 			what_need_to_paint = 7;
-
+			checkedShape = "text";
 			fDraw = FALSE;
 			break;
+		case 32794:
+			what_need_to_paint = 8;
+			checkedShape = "eraser";
+			fDraw = FALSE;
+			break;
+			//next 4 cases use that to create new pen by new pen size
+		case 32789:
+			penSize = 1;
+			hPen = CreatePen(BS_SOLID, 1, brush);
+			break;
+		case 32790:
+			penSize = 2;
+			hPen = CreatePen(BS_SOLID, 2, brush);
+			break;
+		case 32791:
+			penSize = 4;
+			hPen = CreatePen(BS_SOLID, 4, brush);
+			break;
+		case 32792:
+			penSize = 6;
+			hPen = CreatePen(BS_SOLID, 6, brush);
+			break;
+			//that to choose  pen color
+		case 32780:
+			CHOOSECOLOR cc;
+			COLORREF crCustColors[16];
+			cc.lStructSize = sizeof(CHOOSECOLOR);
+			cc.hwndOwner = NULL;
+			cc.hInstance = NULL;
+			cc.rgbResult = brush;
+			cc.lpCustColors = crCustColors;
+			cc.Flags = CC_RGBINIT | CC_FULLOPEN;
+			cc.lCustData = 0L;
+			cc.lpfnHook = NULL;
+			cc.lpTemplateName = NULL;
+			if (ChooseColor(&cc))
+				brush=cc.rgbResult;
+			hPen = CreatePen(BS_SOLID, penSize, brush);
+			break;
+			//that to choose fill for shapes
+		case 32793:
+			cc.lStructSize = sizeof(CHOOSECOLOR);
+			cc.hwndOwner = NULL;
+			cc.hInstance = NULL;
+			cc.rgbResult = brushFill;
+			cc.lpCustColors = crCustColors;
+			cc.Flags = CC_RGBINIT | CC_FULLOPEN;
+			cc.lCustData = 0L;
+			cc.lpfnHook = NULL;
+			cc.lpTemplateName = NULL;
+			if (ChooseColor(&cc))
+				brushFill = cc.rgbResult;
+			break;
+			//use that to save file
+		case 32771:
+		{
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = hWnd;
+			ofn.hInstance = hInst; 
+			ofn.lpstrFilter = L"Metafile (*.emf)\0*.emf\0Все файлы (*.*)\0*.*\0";
+			ofn.nFilterIndex = 1;
+			ofn.lpstrFile = (LPWSTR)fullpath;
+			ofn.nMaxFile = sizeof(fullpath);
+			ofn.lpstrFileTitle = (LPWSTR)filename;
+			ofn.nMaxFileTitle = sizeof(filename);
+			ofn.lpstrInitialDir = (LPWSTR)dir;
+			ofn.lpstrTitle = L"Save file as...";
+			ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_HIDEREADONLY | OFN_EXPLORER;
+			if (GetSaveFileName(&ofn))
+			{
+				HDC hdc = CreateEnhMetaFile(NULL, (LPCWSTR)fullpath, NULL, NULL);
+
+				if (hMTF)
+				{
+					RECT rect;
+					GetClientRect(hWnd, &rect);
+					PlayEnhMetaFile(hdc, hMTF, &rect);
+				}
+
+				for (auto shapeInList : shapesList)
+				{
+					shapeInList->Draw(hdc);
+				}
+
+				DeleteEnhMetaFile(CloseEnhMetaFile(hdc));
+			}
+		}break;
+		//use that to open file
+		case 32772:
+		{
+			ofn.lStructSize = sizeof(OPENFILENAME);
+			ofn.hwndOwner = hWnd;
+			ofn.hInstance = hInst; 
+			ofn.lpstrFilter = L"Metafile (*.emf)\0*.emf\0Все файлы (*.*)\0*.*\0";
+			ofn.nFilterIndex = 1;
+			ofn.lpstrFile = (LPWSTR)fullpath;
+			ofn.nMaxFile = sizeof(fullpath);
+			ofn.lpstrFileTitle = (LPWSTR)filename;
+			ofn.nMaxFileTitle = sizeof(filename);
+			ofn.lpstrInitialDir = (LPWSTR)dir;
+			ofn.lpstrTitle = L"Save file as...";
+			ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_HIDEREADONLY | OFN_EXPLORER;
+			if (GetOpenFileName(&ofn))
+			{
+				HDC hdc = CreateEnhMetaFile(NULL, NULL, NULL, NULL);
+				hMTF = GetEnhMetaFile((LPWSTR)fullpath);
+				shapesList.clear();
+				if (hMTF)
+				{
+					RECT rect;
+					GetClientRect(hWnd, &rect);
+					PlayEnhMetaFile(hdc, hMTF, &rect);
+				}
+
+			
+			}
+		}break;
+		case ID_PRINT:
+			//use that to print all shapes
+		{
+			PRINTDLG pd;
+			HBITMAP      hbmMem;
+			HANDLE       hOld;
+			static HDC hBitmapDC = 0;
+			ZeroMemory(&pd, sizeof(pd));
+			pd.lStructSize = sizeof(pd);
+			pd.hwndOwner = hWnd;
+			pd.hDevMode = NULL; 
+			pd.hDevNames = NULL; 
+			pd.Flags = PD_USEDEVMODECOPIESANDCOLLATE | PD_RETURNDC;
+			pd.nCopies = 1;
+			pd.nFromPage = 0xFFFF;
+			pd.nToPage = 0xFFFF;
+			pd.nMinPage = 1;
+			pd.nMaxPage = 0xFFFF;
+
+
+			if (PrintDlg(&pd) == TRUE)
+			{
+				
+
+				DOCINFO  document_Information;
+				HDC hPrinter = pd.hDC;
+
+				memset(&document_Information, 0, sizeof(document_Information));
+				document_Information.cbSize = sizeof(document_Information);
+				StartDoc(hPrinter, &document_Information);
+
+				StartPage(hPrinter);
+				if (hMTF)
+				{
+					RECT rect;
+					GetClientRect(hWnd, &rect);
+					PlayEnhMetaFile(hPrinter, hMTF, &rect);
+				}
+				for (auto shapeInList : shapesList)
+				{
+					shapeInList->Draw(hPrinter);
+				}
+				EndPage(hPrinter);
+
+				EndDoc(hPrinter);
+				DeleteDC(hPrinter);
+			}
+		}break;
+		//if we check zoom
+		case 32799:
+		{
+			needToZoom = true;
+		}break;
+		//if we check pen
+		case 32800:
+		{
+			needToZoom = false;
+		}break;
 		default:
 			return DefWindowProc(hWnd, message, wParam, lParam);
 		}
 	}
 	break;
-	case WM_LBUTTONDOWN:
-		shape = animalFactory.get(checkedShape)();
-		if (what_need_to_paint != 2) {
-			shape->SetStartPosition(LOWORD(lParam), HIWORD(lParam));
-			shape->SetEndPosition(LOWORD(lParam), HIWORD(lParam));
-		}
-		else
+	//use for text
+	case WM_CHAR:
+		switch (wParam)
 		{
-			shape->AddPointToArray(LOWORD(lParam), HIWORD(lParam));
-			shape->Draw(hdc, hWnd);
+		case VK_RETURN:
+			size = 0;
+			break;
+		case VK_SPACE:
+			size = 0; break;
+		default:
+				text[size] = (char)wParam;
+				size++;
+				if(shape!=NULL)
+				shape->SetText(wParam, size);
+			break;
 		}
+		//if l button down need to create new shape
+	case WM_LBUTTONDOWN:
+		if(fDraw)
+			if (what_need_to_paint==7)
+			{
+				fDraw == false;
+			}
+		if (!fDraw) {
+			if ((what_need_to_paint != 2) && (what_need_to_paint != 6)&& (what_need_to_paint != 3) && (what_need_to_paint != 7) && (what_need_to_paint != 8)) {
+				shape = shapesFactory.get(checkedShape)();
+				shape->SetStartPosition(LOWORD(lParam), HIWORD(lParam));
+				shape->SetEndPosition(LOWORD(lParam), HIWORD(lParam));
+			}
+			else if(what_need_to_paint != 7)
+			{
+				shape = shapesFactory.get(checkedShape)();
+				shape->AddPointToArray(LOWORD(lParam), HIWORD(lParam));
+			}
+			else
+			{
+				shape = shapesFactory.get(checkedShape)();
+				shape->SetStartPosition(LOWORD(lParam), HIWORD(lParam));
+			}
+			if (what_need_to_paint != 8)
+				shape->SetHpen(hPen);
+			else
+				shape->SetHpen(CreatePen(BS_SOLID, penSize, RGB(255, 255, 255)));
+			
+				shape->SetBrush(brushFill);
+				fDraw = TRUE;
 			shapesList.push_back(shape);
-		fDraw = TRUE;
-		ptPrevious.x = LOWORD(lParam);
-		ptPrevious.y = HIWORD(lParam);
+		}
+		
 		break;
+		//add new point for some shapes if need
 	case WM_LBUTTONUP:
+		SetRect(&rec, LOWORD(lParam), HIWORD(lParam), LOWORD(lParam) + 200, HIWORD(lParam) + 200);
 		switch (what_need_to_paint)
 		{
-		case 6:if (fDraw)
+		case 3:case 6:if (fDraw)
 		{
-			hdc = GetDC(hWnd);
-			POINT temp[1];
-			temp[0].x = LOWORD(lParam);
-			temp[0].y = HIWORD(lParam);
-			pt.push_back(temp[0]);
-			points_num++;
-			Polygon(hdc, pt.data(), points_num);
-			ReleaseDC(hWnd, hdc);
+			shape->AddPointToArray(LOWORD(lParam), HIWORD(lParam));
 		}
+			   break;
+		case 7:{	shape->SetStartPosition(LOWORD(lParam), HIWORD(lParam)); 
+}
+			   break;
 		default:
 			DeleteObject(&shape);
 			fDraw = FALSE;
 			break;
 		}
 		break;
+		//change last position for shapes
 	case WM_MOUSEMOVE:
 		switch (what_need_to_paint)
 		{
-		case 2:
+			case 2:case 8:
 			if (fDraw)
 			{
-				
 				shape->AddPointToArray(LOWORD(lParam), HIWORD(lParam));
-				
 			}
 			break;
-		case 6:
+		case 3:case 6:
 			if (fDraw)
 			{
-				hdc = GetDC(hWnd);
-				POINT temp[1];
-				temp[0].x = LOWORD(lParam);
-				temp[0].y = HIWORD(lParam);
-				Polygon(hdc, pt.data(), points_num);
-				ReleaseDC(hWnd, hdc);
+				
 			}
 			break;
 		default:if (fDraw)
 		{
-			hdc = GetDC(hWnd);
 			shape->SetEndPosition(LOWORD(lParam), HIWORD(lParam));
-			shape->Draw(hdc, hWnd);
-			ReleaseDC(hWnd, hdc);
 			break;
 		}
 		}
-
 		break;
-
+		//load all shapes on device context with double buffing
 	case WM_PAINT:
-	{PAINTSTRUCT ps;
+	{
+	PAINTSTRUCT ps;
 	HDC hdc, hdcOld;
 	hdc = BeginPaint(hWnd, &ps);
-	hdcOld = CreateCompatibleDC(hdc);
+	RECT rect;
+	GetClientRect(hWnd, &rect);
+	hdcOld = CreateCompatibleDC(hdc); 
 	HBITMAP hBitmap = CreateCompatibleBitmap(hdc,
 		ps.rcPaint.right - ps.rcPaint.left,
 		ps.rcPaint.bottom - ps.rcPaint.top);
@@ -318,23 +602,41 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	SetBkMode(hdcOld, TRANSPARENT);
 	FillRect(hdcOld, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
 	SelectObject(hdc, hPen);
+
+	GetClientRect(hWnd, &rect);
+	if (hMTF)
+	{
+		RECT rect;
+		GetClientRect(hWnd, &rect);
+		rect.left = 0;
+		rect.top = 0;
+		rect.right = 1500;
+		rect.bottom = 700;
+		PlayEnhMetaFile(hdcOld, hMTF, &rect);
+	}
 	for (auto shapeInList : shapesList)
 	{
-		shapeInList->Draw(hdcOld, hWnd);
+		shapeInList->Draw(hdcOld);
 	}
-	 BitBlt(hdc, ps.rcPaint.left, ps.rcPaint.top, ps.rcPaint.right - ps.rcPaint.left, ps.rcPaint.bottom - ps.rcPaint.top, hdcOld, 0, 0, SRCCOPY);
-
+	if (needToFill)
+		FillRect(hdc, &rect, WHITE_BRUSH);
+		StretchBlt(hdc, ps.rcPaint.left, ps.rcPaint.top, (ps.rcPaint.right - ps.rcPaint.left) / coefficientOfSizeWidth, (ps.rcPaint.bottom - ps.rcPaint.top) / coefficientOfSizeHeight, hdcOld, 0 + shearX, 0 + shearY, (ps.rcPaint.right - ps.rcPaint.left), (ps.rcPaint.bottom - ps.rcPaint.top), SRCCOPY);
+	
+	needToFill = false; 
 	SelectObject(hdcMem, hdcOld);
 	DeleteDC(hdcOld);	
 	DeleteObject(hBitmap);
 	EndPaint(hWnd, &ps);
 	}
 		break;
-	case WM_RBUTTONDOWN:
-		hdc = GetDC(hWnd);
-		Polygon(hdc, pt.data(), points_num);
-		ReleaseDC(hWnd, hdc);
+	//use for text,polygone,polyline that to set end of shape drawing
+	case WM_RBUTTONDOWN: {
+		DeleteObject(shape);
+		size = 0;
+		fDraw = false;
+	}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
